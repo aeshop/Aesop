a8cb77f0b0aebc60e6c42b8412ac77886f38ee96
RyuKwanHyun <sosto258@naver.com>
2021-12-22 02:43:03 +0900

    Login JSP Update v1

--- src/main/java/teamSemiProject2/edu/kh/semi/member/model/service/MemberService.java
+++ src/main/java/teamSemiProject2/edu/kh/semi/member/model/service/MemberService.java
@@ -1,5 +1,25 @@
 package teamSemiProject2.edu.kh.semi.member.model.service;
 
+import static teamSemiProject2.edu.kh.semi.common.JDBCTemplate.*;
+
+import java.sql.Connection;
+
+import teamSemiProject2.edu.kh.semi.member.model.dao.MemberDAO;
+import teamSemiProject2.edu.kh.semi.member.model.vo.Member;
+
 public class MemberService {
+
+	private MemberDAO dao = new MemberDAO();
+	
+	
+	public Member login(String memberId, String memberPw) throws Exception{
+		Connection conn = getConnection();
+		
+		Member loginMember = dao.login(memberId, memberPw, conn);
+		
+		close(conn);
+		
+		return loginMember;
+	}
 
 }
