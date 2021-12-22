e4b8984993f885177ca4a64e93a90031b307ccb0
zxjk78 <zxjk78@gmail.com>
2021-12-22 11:30:07 +0900

    before merge upload mybranch

---
+++ src/main/java/teamSemiProject2/edu/kh/semi/member/controller/MemberController.java
@@ -0,0 +1,105 @@
+package teamSemiProject2.edu.kh.semi.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import teamSemiProject2.edu.kh.semi.member.model.service.MemberService;
+import teamSemiProject2.edu.kh.semi.member.model.vo.Member;
+
+@WebServlet("/member/*")
+public class MemberController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String method = req.getMethod();
+		String uri = req.getRequestURI();
+		String contextPath = req.getContextPath();
+		
+		String command = uri.substring(  (contextPath + "/member/").length()  );
+				
+		String path = null;
+		RequestDispatcher dispatcher = null;
+		
+			if(command.equals("login")) {
+				if(method.equals("GET")) {
+					path = "/WEB-INF/views/member/login.jsp";
+					dispatcher = req.getRequestDispatcher(path);
+					dispatcher.forward(req, resp);
+				}else {
+					// POST
+					req.setCharacterEncoding("UTF-8");
+					
+					// 전달받은 파라미터를 변수에 저장
+					String memberId = req.getParameter("memberId");
+					String memberPw = req.getParameter("memberPw");
+					
+					System.out.println("memberId : " + memberId);
+					System.out.println("memberPw : " + memberPw);
+					
+					try {
+						MemberService service = new MemberService();
+						
+						Member loginMember = service.login(memberId, memberPw);
+						
+						HttpSession session = req.getSession();
+						
+						if (loginMember != null) {
+							System.out.println("여기까지");
+
+							if (loginMember.getStatusCode() == 101) {
+								System.out.println(loginMember.getMemberEmail());
+								session.setAttribute("loginMember", loginMember);
+								session.setMaxInactiveInterval(3000);
+								
+							}
+							
+						}else { // 로그인 실패
+							session.setAttribute("message", "아이디 또는 비밀번호를 확인해주세요.");
+							
+						}
+						resp.sendRedirect(req.getContextPath() );
+						
+					}catch (Exception e) {
+						e.printStackTrace();
+					}
+					
+				}
+				
+			}
+//			else if(command.equals("join")) {
+//				if(method.equals("GET")) {
+//					path = "/WEB-INF/views/member/join.jsp";
+//					dispatcher = req.getRequestDispatcher(path);
+//					dispatcher.forward(req, resp);
+//				}else {
+//					// POST
+//					
+//					
+//					
+//					
+//				}
+//				
+//				
+//				
+//				
+//				
+//			}
+			
+		
+		
+		
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/member/login.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<link rel="stylesheet" href="${contextPath}/resources/css/member/member.css">
+<jsp:include page="../common/r_header.jsp" />
+
+<div class="login_wrap">
+	<form id="login" method="post" action="${contextPath}/member/login" onsubmit="return loginValidate()" >
+		<div class="login_container">
+			<div class="login_container_form">
+				<div class="login_title">LOGIN</div>
+				<fieldset>
+					<div class="login_form">
+						<div class="login_form_input">
+							<input type="text" id="memberId" name="memberId" placeholder="ID" required>
+							<input type="password" id="memberPw" name="memberPw" placeholder="PW" required>
+						</div>
+						<button type="submit" class="login_btn">로그인</button>
+						<div class="typeLogin">
+							<a href="#">아이디찾기</a> <a href="#">비밀번호찾기</a>
+						</div>
+					</div>
+				</fieldset>
+			</div>
+
+
+			<div class="login_container_img"></div>
+		</div>
+	</form>
+
+</div>
+
+
+
+<!-- footer -->
+<jsp:include page="../common/r_footer.jsp" />
+
+
+</body>
+</html>
